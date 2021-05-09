553f9a748835d77eda343aece2fc42c4ba2ce694
Flor Santos <florsantosbueno@gmail.com>
2021-05-09 21:59:35 +0200

    Estructura inicial presentación
    Estructura inicial presentación y pruebas de comunicación con el servidor

---
+++ workspace-presentacion/Flotix2021/Flotix2021/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flotix2021
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GetItems();
+        }
+
+        private static void GetItems()
+        {
+            var url = $"http://localhost:8090/users";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+                            // Do something with responseBody
+                            Console.WriteLine(responseBody);
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                // Handle error
+                Console.WriteLine("ERROR EN LA CONEXION");
+            }
+        }
+
+    }
+}
